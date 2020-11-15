e37831e01e69b3c6b0e6e5dbe369e90398be0d21
mchudz97 <mchudz97@gmail.com>
2020-11-15 22:44:44 +0100

    1: djoser initial implementation

--- tabletop_connector_api/urls.py
+++ tabletop_connector_api/urls.py
@@ -14,9 +14,9 @@ router.register(r'users', UserCreateViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
+   # path('api-token-auth/', views.obtain_auth_token),
+   # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/auth/', include('djoser.urls.authtoken')),
     # the 'api-root' from django rest-frameworks default router
     # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
     re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
