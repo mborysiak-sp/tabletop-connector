0a35fd56f59f5d129757c8b5603b7e5ec178b87e
mchudz97 <mchudz97@gmail.com>
2020-11-16 19:06:14 +0100

    1: djoser models todo

--- tabletop_connector_api/users/models.py
+++ tabletop_connector_api/users/models.py
@@ -1,10 +1,7 @@
 import uuid
 from django.db import models
-from django.conf import settings
-from django.dispatch import receiver
 from django.contrib.auth.models import AbstractUser
-from django.db.models.signals import post_save
-from rest_framework.authtoken.models import Token
+
 
 
 class User(AbstractUser):
@@ -14,7 +11,6 @@ class User(AbstractUser):
         return self.username
 
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
+class Game(models.Model):
+    id = models.IntegerField(primary_key=True)
+    #users = models.ForeignKey(User, on_delete=)
