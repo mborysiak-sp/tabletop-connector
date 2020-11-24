b0efc89089adfcb0ba16bbe839efe28395e6d943
mchudz97 <mchudz97@gmail.com>
2020-11-24 17:45:16 +0100

    4: Geocoding function tested + refactor

--- tabletop_connector_api/events/test/test_utils.py
+++ tabletop_connector_api/events/test/test_utils.py
@@ -1,18 +1,25 @@
 from tabletop_connector_api.events.utils import address_to_geocode
 
 
-class Geocode_test:
+def test_valid_address():
+    valid_dict = {
+        "country": "Poland",
+        "city": "Gdansk",
+        "street": "Wita Stwosza",
+        "postal_code": "80-306",
+        "number": "57"
+    }
 
-    def test_valid_address(self):
-        valid_dict = {
-            "country": "Poland",
-            "city": "Gdansk",
-            "street": "Wita Stwosza",
-            "postal_code": "80-306",
-            "number": "57"
-        }
+    assert address_to_geocode(valid_dict) == (54.395704550000005, 18.5739726651911)
 
-        assert address_to_geocode(valid_dict) is None
 
-    def test_invalid_address(self):
-        pass
+def test_invalid_address():
+    valid_dict = {
+        "country": "xD",
+        "city": "xD",
+        "street": "xD",
+        "postal_code": "21-037",
+        "number": "222"
+    }
+
+    assert address_to_geocode(valid_dict) == ()
--- tabletop_connector_api/events/utils.py
+++ tabletop_connector_api/events/utils.py
@@ -3,8 +3,13 @@ from geopy.geocoders import Nominatim
 
 def address_to_geocode(address: dict):
 
-    return Nominatim().geocode(address.get("country") + " "
+    result = Nominatim(user_agent="xd").geocode(address.get("country") + " "
                                + address.get("city") + " "
                                + address.get("postal_code") + " "
                                + address.get("street") + " "
                                + address.get("number"))
+
+    try:
+        return result.latitude, result.longitude
+    except AttributeError:
+        return ()
