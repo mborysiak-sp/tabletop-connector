130fe198893383622aa5770f1f8d44e758dbf885
marcin borysiak <marcin.borysiak@profil-software.com>
2020-11-24 10:51:12 +0100

    4: Remove configurations module leftovers

--- manage.py
+++ manage.py
@@ -5,24 +5,16 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tabletop_connector_api.config")
-    os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
-
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tabletop_connector_api.config')
     try:
-        from configurations.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django  # noqa
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)
 
 
