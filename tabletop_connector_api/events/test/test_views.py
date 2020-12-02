9c7872e2d7c606adeff999c16ee2c1444306cb65
mchudz97 <mchudz97@gmail.com>
2020-12-02 18:30:31 +0100

    5: Changed views

--- tabletop_connector_api/events/test/test_views.py
+++ tabletop_connector_api/events/test/test_views.py
@@ -1,42 +1,85 @@
 import pytest
+from django.test import TestCase
+from django.urls import reverse
 from rest_framework.test import APIRequestFactory
+
+from tabletop_connector_api.events.models import Event
+from tabletop_connector_api.events.serializers import EventSerializer
 from tabletop_connector_api.events.test.factories import EventFactory, AddressFactory
-from tabletop_connector_api.events.views import CustomEventViewSet
+from tabletop_connector_api.events.views import CustomEventViewSet, EventViewSet
 
 
 @pytest.mark.django_db
-class TestCustomEventViewSet:
+class TestCustomEventViewSet(TestCase):
+
+    def setUp(self):
+        self.view = CustomEventViewSet.as_view()
+        self.factory = APIRequestFactory()
 
     def test_CustomEventViewSet_found_response_code(self):
-        view = CustomEventViewSet.as_view()
-        factory = APIRequestFactory()
         EventFactory.create(address=AddressFactory(geo_x=54.34950, geo_y=18.64847))
-        request = factory \
+        request = self.factory \
             .get('api/geteventbydistance/?distance=10&country=Poland&city=Gdansk&street=Teatralna')
 
-        assert view(request).status_code == 302
+        assert self.view(request).status_code == 200
 
     def test_CustomEventViewSet_found_in_queryset(self):
-        view = CustomEventViewSet.as_view()
-        factory = APIRequestFactory()
         EventFactory.create(address=AddressFactory(geo_x=54.34950, geo_y=18.64847))
-        request = factory \
+        request = self.factory \
             .get('api/geteventbydistance/?distance=10&country=Poland&city=Gdansk&street=Teatralna')
 
-        assert len(view(request).data) == 1
+        assert len(self.view(request).data) == 1
 
     def test_CustomEventViewSet_not_found_response_code(self):
-        view = CustomEventViewSet.as_view()
-        factory = APIRequestFactory()
-        request = factory \
+        request = self.factory \
             .get('api/geteventbydistance/?distance=10&country=Poland&city=Gdansk&street=Teatralna')
 
-        assert view(request).status_code == 404
+        assert self.view(request).status_code == 204
 
     def test_CustomEventViewSet_when_address_no_specified(self):
-        view = CustomEventViewSet.as_view()
-        factory = APIRequestFactory()
-        request = factory \
+        request = self.factory \
             .get('api/geteventbydistance/?distance=10')
 
-        assert view(request).status_code == 404
+        assert self.view(request).status_code == 204
+
+
+# @pytest.mark.django_db
+# class TestEventViewSet(TestCase):
+#
+#     def setUp(self):
+#         self.factory = APIRequestFactory()
+#         self.view = EventViewSet.as_view({'get': 'list', 'post': 'create'})
+#         self.example = {'name': 'test',
+#                         'date': '2020-12-10T16:01:00+0000',
+#                         'creator': 'null',
+#                         'address': {
+#                             'country': 'Poland',
+#                             'city': 'Gdansk',
+#                             'street': 'Wita Stwosza',
+#                             'postal_code': '21-307',
+#                             'number': '2',
+#                             'geo_x': 'null',
+#                             'geo_y': 'null'
+#                         },
+#                         'chat': 'null'}
+#
+#     def test_get_all_events(self):
+#         EventFactory()
+#         EventFactory(name='test2')
+#         request = self.factory.get('api/event/')
+#         events = Event.objects.all()
+#         serializer = EventSerializer(events, many=True)
+#         self.assertEqual(self.view(request).data['count'], len(serializer.data))
+#
+#     def test_create_valid_event(self):
+#         request = self.factory.post('api/event/', self.example, format='json')
+#
+#
+#         assert self.view(request).status_code == 200
+#         assert Event.objects.count() == 1
+#
+#     def test_create_invalid_event(self):
+#         incorrect_example = self.example.copy()
+#         incorrect_example['name'] = '*'*100
+#         request = self.factory.put('api/event/', incorrect_example, format='json')
+#         assert Event.objects.count() == 0
--- tabletop_connector_api/events/views.py
+++ tabletop_connector_api/events/views.py
@@ -1,25 +1,92 @@
-# Create your views here.
-
-from rest_framework import viewsets, generics, mixins, status
-from rest_framework.generics import ListAPIView
+from rest_framework import viewsets, status
+from rest_framework.generics import ListAPIView, get_object_or_404
 from rest_framework.response import Response
 
 from .filters import FilterByDistance
 from .models import Address, Event
 from .serializers import AddressSerializer, EventSerializer
-from .utils import address_to_geocode, get_distance_in_kilometers
 
 
-class AddressViewSet(viewsets.ModelViewSet):
-    queryset = Address.objects.all()
+class AddressViewSet(viewsets.ViewSet):
     serializer_class = AddressSerializer
+    authentication_classes = ()
+    permission_classes = ()
+
+    def list(self, request):
+        serializer = self.serializer_class(Address.objects.all(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        address = get_object_or_404(Address.objects.all(), pk=pk)
+        return Response(self.serializer_class(address).data, status=status.HTTP_200_OK)
+
+    def update(self, request, pk=None):
+        address = Address.objects.get(pk=pk)
+        serializer = self.serializer_class(address, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        address = Address.objects.get(pk=pk)
+        serializer = self.serializer_class(address, data=request.data)
+        if serializer.is_valid():
+            address.delete()
+            return Response(serializer.data, status=status.HTTP_410_GONE)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class EventViewSet(viewsets.ModelViewSet):
-    queryset = Event.objects.all()
+class EventViewSet(viewsets.ViewSet):
     serializer_class = EventSerializer
     authentication_classes = ()
     permission_classes = ()
+
+    def list(self, request):
+        serializer = EventSerializer(Event.objects.all(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        event = get_object_or_404(Event.objects.all(), pk=pk)
+        return Response(self.serializer_class(event).data, status=status.HTTP_200_OK)
+
+    def update(self, request, pk=None):
+        event = Event.objects.get(pk=pk)
+        serializer = self.serializer_class(event, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        event = Event.objects.get(pk=pk)
+        serializer = self.serializer_class(event, data=request.data)
+        if serializer.is_valid():
+            event.delete()
+            return Response(serializer.data, status=status.HTTP_410_GONE)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CustomEventViewSet(ListAPIView):
@@ -38,15 +105,13 @@ class CustomEventViewSet(ListAPIView):
 
         queryset = self.get_queryset()
         if queryset.count() == 0:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             serializer = self.serializer_class(queryset, many=True)
-            return Response(data=serializer.data, status=status.HTTP_302_FOUND)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-    # def list(self, request):
-    # events = Event.objects.all()
-    # serializer = EventSerializer(events, many=True)
-    # return Response(serializer.data)
+
+
 
     # def create(self, request):
     #     serializer = EventSerializer(data=request.data)
