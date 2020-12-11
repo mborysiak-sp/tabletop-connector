cf880fc021793d8efc641a5fcfa63d71b3fccd2c
Marcin Borysiak <marcinez24@gmail.com>
2020-12-12 00:47:11 +0100

    6: Refactor games loading

--- tabletop_connector_api/events/utils.py
+++ tabletop_connector_api/events/utils.py
@@ -6,7 +6,6 @@ import pandas as pd
 from geopy.geocoders import Nominatim
 
 from tabletop_connector_api.events.models import Game
-from tabletop_connector_api.config.local import BASE_DIR
 
 
 def address_to_geocode(address: dict):
@@ -37,18 +36,22 @@ def get_distance_in_kilometers(from_lat, from_long, to_lat, to_long):
 
 def load_games():
     """This function loads database exported from BGG into the project"""
-    print('Loading games...')
-    df = pd.read_csv(os.path.join('data', 'games.csv'))
-    df.dropna(inplace=True)
-    row_iter = df.iterrows()
-    games = [
-        Game(name=row['name'],
-             image=row['image'],
-             thumbnail=row['thumbnail'],
-             min_players=row['min_players'],
-             max_players=row['max_players'],
-             playtime=row['play_time'])
-        for index, row in row_iter
-    ]
-    Game.objects.bulk_create(games)
-    print('Games loaded')
+    if not Game.objects.exists():
+        try:
+            print('Trying to load games games...')
+            df = pd.read_csv(os.path.join('data', 'games.csv'))
+        except FileNotFoundError:
+            print('File not found')
+        df.dropna(inplace=True)
+        row_iter = df.iterrows()
+        games = [
+            Game(name=row['name'],
+                 image=row['image'],
+                 thumbnail=row['thumbnail'],
+                 min_players=row['min_players'],
+                 max_players=row['max_players'],
+                 playtime=row['play_time'])
+            for index, row in row_iter
+        ]
+        Game.objects.bulk_create(games)
+        print('Games loaded')
