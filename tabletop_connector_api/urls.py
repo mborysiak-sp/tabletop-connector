dac453552d30e13176db652275ef0cfae8c81c23
Marcin Borysiak <marcinez24@gmail.com>
2020-12-12 10:18:24 +0100

    8: Add swagger implementation
    Old documentation files have been deleted

--- tabletop_connector_api/urls.py
+++ tabletop_connector_api/urls.py
@@ -1,12 +1,32 @@
+from django.conf.urls import url
 from django.urls import path, include
 from django.contrib import admin
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from .users import urls as user_urls
 from .events import urls as event_urls
 
-app_name = 'api'
+schema_view = get_schema_view(
+    openapi.Info(
+        title='API',
+        default_version='v1',
+        description='Test description',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='temp@email.'),
+        license=openapi.License(name='BSD License'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(user_urls)),
-    path('api/', include(event_urls))
+    path('api/', include(event_urls)),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=1000), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=1000), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=1000), name='schema-redoc'),
 ]
