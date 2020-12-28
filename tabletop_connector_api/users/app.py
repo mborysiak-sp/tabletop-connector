4e5265d31110659f2aadc34ecac932d2d9cb9155
mborysiak <marcin.borysiak@profil-software.com>
2020-12-28 22:54:32 +0100

    17: Delete signal for person

--- tabletop_connector_api/users/app.py
+++ tabletop_connector_api/users/app.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'tabletop_connector_api.users'
-
-    def ready(self):
-        import tabletop_connector_api.users.signals
