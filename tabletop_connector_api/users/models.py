8758f03c9ea6b25cf4b7801236a8bd70ee959f0d
mchudz97 <mchudz97@gmail.com>
2020-12-29 20:19:32 +0100

    13: default avatar ;)))

--- tabletop_connector_api/users/models.py
+++ tabletop_connector_api/users/models.py
@@ -17,5 +17,5 @@ class Profile(models.Model):
     firstname = models.TextField(blank=True, null=True, max_length=64)
     lastname = models.CharField(blank=True, null=True, max_length=64)
     date_joined = models.DateTimeField(auto_now_add=True)
-    avatar = models.ImageField(upload_to='avatars/', default='default_avatar.png')
+    avatar = models.ImageField(upload_to='avatars/', default='./avatars/default_avatar.png', )
 
