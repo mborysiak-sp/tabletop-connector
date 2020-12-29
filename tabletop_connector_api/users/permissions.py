4b83c90eec8b0139320591459b77e7f0f023deb4
mborysiak <marcin.borysiak@profil-software.com>
2020-12-29 19:51:32 +0100

    14: Fix avatar field

--- tabletop_connector_api/users/permissions.py
+++ tabletop_connector_api/users/permissions.py
@@ -19,3 +19,8 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.user == request.user
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
--- tabletop_connector_api/users/urls.py
+++ tabletop_connector_api/users/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 
 from django.urls import path, include
 
-from .views import ProfileViewSet, return_me
+from .views import ProfileViewSet, ProfileMeAPIView
 
 router = DefaultRouter()
 router.register(r'profiles', ProfileViewSet)
@@ -12,7 +12,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.urls.authtoken')),
-    path('profiles/me/', return_me)
+    path('profiles/me/', ProfileMeAPIView.as_view())
 ]
 
 urlpatterns += router.urls
